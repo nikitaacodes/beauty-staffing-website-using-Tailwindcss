d5fee270d31063fa51ef2993babb726814411d5f
nikitaacodes <nikitaa4822@gmail.com>
2024-12-15 19:05:29 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./main.html'],
+  theme: {
+    extend: {
+      colors:{
+   'grey': '#454444'
+      },
+      fontFamily: {
+        'astyle': ['poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
